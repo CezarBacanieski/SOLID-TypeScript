5447beb35688dc6d7cc22018ff9542440be71df4
CezarDev <bacanieskidev@gmail.com>
2024-09-03 18:42:59 -0300

    feat: aplicacao de responsabilidade unica aplicada

--- tarefa-1/main.ts
+++ tarefa-1/main.ts
@@ -1,20 +1,28 @@
-import Colaborador from "./Colaborador";
-import Sistema from "./QuadroColaboradores";
-import { Cargos } from "./enum/cargos";
+import Colaborador from './Colaborador';
+import QuadroColaboradores from './QuadroColaboradores';
+import { Cargos } from './enum/cargos';
+import Pagamento from './Pagamento';
+import GeraRelatorio from './GeraRelatorio';
+import CalculaSalario from './CalculaSalario';
 
-const sistema = new Sistema();
+const quadroColaboradores = new QuadroColaboradores();
+const calculaSalario = new CalculaSalario();
+const pagamento = new Pagamento(calculaSalario);
+const gerarRelatorio = new GeraRelatorio(
+  calculaSalario,
+  quadroColaboradores.colaboradores
+);
 
+const colaborador1 = new Colaborador('José', Cargos.Estagiario);
+const colaborador2 = new Colaborador('Maria', Cargos.Junior);
+const colaborador3 = new Colaborador('João', Cargos.Pleno);
 
-const colaborador1 = new Colaborador("José", Cargos.Estagiario);
-const colaborador2 = new Colaborador("Maria", Cargos.Junior);
-const colaborador3 = new Colaborador("João", Cargos.Pleno);
+quadroColaboradores.contratarColaborador(colaborador1);
+quadroColaboradores.contratarColaborador(colaborador2);
+quadroColaboradores.contratarColaborador(colaborador3);
 
-sistema.contratarColaborador(colaborador1);
-sistema.contratarColaborador(colaborador2);
-sistema.contratarColaborador(colaborador3);
-
-console.log(sistema.gerarRelatorioJSON());
+console.log(gerarRelatorio.gerarJSON());
 
 console.log(colaborador1);
-sistema.pagaColaborador(colaborador1);
+pagamento.pagar(colaborador1);
 console.log(colaborador1);
